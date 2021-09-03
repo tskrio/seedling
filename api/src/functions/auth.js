6771a5eb05d14c67c4f2100eb932b1874bdf98c6
Jace <638764+jacebenson@users.noreply.github.com>
2021-09-03 18:52:39 -0500

    upgrading to 36.3

--- api/src/functions/auth.js
+++ api/src/functions/auth.js
@@ -3,33 +3,19 @@ import { DbAuthHandler } from '@redwoodjs/api'
 
 export const handler = async (event, context) => {
   try {
-    console.log('in auth.js');
-    console.log(event);
-    console.log(context);
+    console.log('in auth.js')
+    console.log(event)
+    console.log(context)
     const authHandler = new DbAuthHandler(event, context, {
       // Provide prisma db client
       db: db,
-
-      // The name of the property you'd call on `db` to access your user table.
-      // ie. if your Prisma model is named `User` this value would be `user`, as in `db.user`
       authModelAccessor: 'user',
-
-      // A map of what dbAuth calls a field to what your database calls it.
-      // `id` is whatever column you use to uniquely identify a user (probably
-      // something like `id` or `userId` or even `email`)
       authFields: {
         id: 'id',
         username: 'email',
         hashedPassword: 'hashedPassword',
         salt: 'salt',
       },
-
-      // Whatever you want to happen to your data on new user signup. Redwood will
-      // check for duplicate usernames before calling this handler. At a minimum
-      // you need to save the `username`, `hashedPassword` and `salt` to your
-      // user table. `userAttributes` contains any additional object members that
-      // were included in the object given to the `signUp()` function you got
-      // from `useAuth()`
       signupHandler: ({ username, hashedPassword, salt, userAttributes }) => {
         if (!username.match(/.+?@.+?\..+?/)) {
           throw new Error('Not a valid email address!')
@@ -43,9 +29,10 @@ export const handler = async (event, context) => {
           },
         })
       },
-
-      // How long a user will remain logged in, in seconds
       loginExpires: 60 * 60 * 24 * 365 * 10,
+      loginHandler: (user) => {
+        return user
+      },
     })
 
     return authHandler.invoke()
