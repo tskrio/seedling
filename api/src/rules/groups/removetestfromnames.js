8418067fdeb11c94d9030abf4d7afbef26db5b8d
Jace <638764+jacebenson@users.noreply.github.com>
2021-09-20 23:09:15 -0500

    added rule handlers to group service

---
+++ api/src/rules/groups/removetestfromnames.js
@@ -0,0 +1,41 @@
+/**
+//import * as util from 'src/lib/util'// How to call reusable code across rules
+//import { db } from 'src/lib/db'// Needed to do other CRUDs on other tables
+ * Properties available to command function
+| DB Action | When   | First Param                    | Second Param         |
+| --------- | ------ | ------------------------------ | -------------------- |
+| Create    | Before | Form input                     | `null`               |
+| Create    | After  | Created record with references | `null`               |
+| Read      | *      | `null`                         | `null`               |
+| Update    | Before | Form input                     | Record before update |
+| Update    | After  | Record after update            | Record before update |
+| Delete    | Before | Record to be deleted           | `null`               |
+| Delete    | After  | Record deleted                 | `null`               |
+ * active dictates if this rule will run
+ * order dictates the order in the list of rules this will run
+ * title is used for log statements
+ * when is an array of when this rule can run expects 'before', or 'after'
+ * type is an array of the type of db action this runs on expects, 'create', 'read', 'update', or 'delete'
+ * file is for debugging
+ * If title,order,when,type,command,active,file are missing rule will not run
+ */
+//import { db } from 'src/lib/db' // Needed to do other CRUDs on other tables
+import { logger } from 'src/lib/logger'
+module.exports = {
+  command: async function (current /*previous*/) {
+    console.log('in removetest rule')
+    try {
+      if (current?.name?.includes('test')) {
+        current.name = current.name.replace('test', '')
+      }
+    } catch (e) {
+      logger.error(e)
+    }
+  },
+  active: false,
+  order: 10,
+  title: 'test rule',
+  when: ['before'],
+  type: ['update', 'create'],
+  file: __filename,
+}
--- api/src/services/groups/groups.js
+++ api/src/services/groups/groups.js
@@ -1,5 +1,8 @@
 import { db } from 'src/lib/db'
 import { requireAuth } from 'src/lib/auth'
+import { logger } from 'src/lib/logger'
+import * as util from 'src/lib/rulesUtil'
+import rules from 'src/rules/groups/**.{js,ts}'
 
 // Used when the environment variable REDWOOD_SECURE_SERVICES=1
 export const beforeResolver = (rules) => {
@@ -22,42 +25,130 @@ export const beforeResolver = (rules) => {
   })
 }
 
-export const groups = () => {
-  return db.group.findMany({
+export const groups = async () => {
+  let records = await db.group.findMany({
     include: {
       GroupRole: true,
       GroupMember: true,
     },
   })
+  let beforeReadRulesArr = util.loadRules(rules, 'before', 'read')
+  let readRecords = records.map((current) => {
+    beforeReadRulesArr.forEach((rule) => {
+      logger.info(`Starting Before Read Rule "${rule.title}" ${rule.order}`)
+      rule.command(current, null)
+      logger.info(`Ending Before Read Rule "${rule.title}"`)
+    })
+    return current
+  })
+  let afterReadRulesArr = util.loadRules(rules, 'after', 'read')
+  records.forEach((current) => {
+    afterReadRulesArr.forEach((rule) => {
+      logger.info(`Starting After Read Rule "${rule.title}" ${rule.order}`)
+      rule.command(current, null)
+      logger.info(`Ending After Read Rule "${rule.title}"`)
+    })
+  })
+  return readRecords
 }
 
-export const group = ({ id }) => {
-  return db.group.findUnique({
+export const group = async ({ id }) => {
+  let beforeReadRulesArr = util.loadRules(rules, 'before', 'read')
+  let current = await db.group.findUnique({
     where: { id },
     include: {
       GroupRole: true,
       GroupMember: true,
     },
   })
+  beforeReadRulesArr.forEach((rule) => {
+    logger.info(`Starting Before Read Rule "${rule.title}" ${rule.order}`)
+    rule.command(current, null)
+    for (var prop in current) {
+      logger.info(`  ${prop} "${current[prop]}"=>"${current[prop]}"`)
+    }
+    logger.info(`Ending Before Read Rule "${rule.title}"`)
+  })
+  let afterReadRulesArr = util.loadRules(rules, 'after', 'read')
+  afterReadRulesArr.forEach((rule) => {
+    logger.info(`Starting After Read Rule "${rule.title}" ${rule.order}`)
+    rule.command(current, null)
+    logger.info(`Ending After Read Rule "${rule.title}"`)
+  })
+  return current
 }
 
-export const createGroup = ({ input }) => {
-  return db.group.create({
+export const createGroup = async ({ input }) => {
+  let beforeCreateRulesArr = util.loadRules(rules, 'before', 'create')
+  beforeCreateRulesArr.forEach((rule) => {
+    logger.info(`Starting Before Create Rule "${rule.title}" ${rule.order}`)
+    rule.command(input, null)
+    for (var prop in input) {
+      logger.info(`  ${prop} "${input[prop]}"=>"${input[prop]}"`)
+    }
+    logger.info(`Ending Before Create Rule "${rule.title}"`)
+  })
+  let create = await db.group.create({
     data: input,
   })
+
+  let afterCreateRulesArr = util.loadRules(rules, 'after', 'create')
+  afterCreateRulesArr.forEach((rule) => {
+    logger.info(`Starting After Create Rule "${rule.title}" ${rule.order}`)
+    rule.command(create, null)
+    logger.info(`Ending After Create Rule "${rule.title}"`)
+  })
+  return create
 }
 
-export const updateGroup = ({ id, input }) => {
-  return db.group.update({
+export const updateGroup = async ({ id, input }) => {
+  let beforeUpdateRulesArr = util.loadRules(rules, 'before', 'update')
+  let previous = await db.group.findUnique({
+    where: { id },
+  })
+  beforeUpdateRulesArr.forEach((rule) => {
+    logger.info(`Starting Before Update Rule "${rule.title}" ${rule.order}`)
+    rule.command(input, previous)
+    for (var prop in input) {
+      logger.info(`  ${prop} "${input[prop]}"=>"${input[prop]}"`)
+    }
+    logger.info(`Ending Before Update Rule "${rule.title}"`)
+  })
+  let update = await db.group.update({
     data: input,
     where: { id },
   })
+
+  let afterUpdateRulesArr = util.loadRules(rules, 'after', 'update')
+  afterUpdateRulesArr.forEach((rule) => {
+    logger.info(`Starting After Update Rule "${rule.title}" ${rule.order}`)
+    rule.command(update, previous)
+    logger.info(`Ending After Update Rule "${rule.title}"`)
+  })
+  return update
 }
 
-export const deleteGroup = ({ id }) => {
-  return db.group.delete({
+export const deleteGroup = async ({ id }) => {
+  let beforeDeleteRulesArr = util.loadRules(rules, 'before', 'delete')
+  let previous = await db.group.findUnique({
     where: { id },
   })
+  beforeDeleteRulesArr.forEach((rule) => {
+    logger.info(`Starting Before Delete Rule "${rule.title}" ${rule.order}`)
+    rule.command(previous, null)
+    logger.info(`Ending Before Delete Rule "${rule.title}"`)
+  })
+  let deleteRecord = await db.group.delete({
+    where: { id },
+  })
+
+  let afterDeleteRulesArr = util.loadRules(rules, 'after', 'delete')
+  afterDeleteRulesArr.forEach((rule) => {
+    logger.info(`Starting After Delete Rule "${rule.title}" ${rule.order}`)
+    rule.command(previous, null)
+    logger.info(`Ending After Delete Rule "${rule.title}"`)
+  })
+  return deleteRecord
 }
 
 export const Group = {
