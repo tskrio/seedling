c9f1c5d7dd8e9e642199d74ca1d34e6679263abb
Jace <638764+jacebenson@users.noreply.github.com>
2021-10-28 00:00:24 +0000

    updated password hashing and limit rules

--- api/src/rules/users/enforcePassword.js
+++ api/src/rules/users/enforcePassword.js
@@ -1,22 +1,16 @@
 import { logger } from 'src/lib/logger'
-import { UserInputError } from '@redwoodjs/graphql-server';
-
 
 module.exports = {
   command: async function (incomingData) {
     try {
-      //console.log('incomingData', incomingData);
       var data = incomingData.hashedPassword;
-      //console.log(`data ${typeof data} ${data}`);
-      if (data.length < 4) {
+      if (data.length === 0){ // if password is empty, remove it.
+        delete incomingData.hashedPassword
+      } else if (data.length < 4) {
         incomingData._error = {
           abort : true,
           message : 'Password not long enough.  Update not saved'
         }
-
-        //console.log('password less than 4');
-        //throw new UserInputError('Password11 not long enough.  Update not saved');
-
       }
     } catch (e) {
       logger.error(e)
@@ -28,6 +22,6 @@ module.exports = {
   title: 'enforce Password',
   when: ['before'],
   type: ['update', 'create'],
-  name: 'hashpassword',
+  name: __filename,
   file: __filename,
 }
