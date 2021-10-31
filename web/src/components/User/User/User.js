c099b38087d3ddd262aba97e42494c21d90fd396
Jace <638764+jacebenson@users.noreply.github.com>
2021-10-31 11:47:45 -0500

    updated user form

--- web/src/components/User/User/User.js
+++ web/src/components/User/User/User.js
@@ -1,3 +1,12 @@
+import {
+  Form,
+  FormError,
+  FieldError,
+  Label,
+  TextField,
+  Submit,
+  PasswordField,
+} from '@redwoodjs/forms'
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 import { Link, routes, navigate } from '@redwoodjs/router'
@@ -45,8 +54,30 @@ const User = ({ user }) => {
     }
   }
 
+  var preferenceFields = []
+  if (user?.preferences) {
+    for (let key in user?.preferences) {
+      console.log(`${key}: ${user?.preferences[key]}`)
+      preferenceFields.push(
+        <label
+          key={key}
+          className="flex border-b border-gray-200 h-12 py-3 items-center"
+        >
+          <span className="text-right px-2">{key}</span>
+          <input
+            name={'preference.' + key}
+            className="focus:outline-none px-3 w-full"
+            placeholder="Card number MM/YY CVC"
+            value={user?.preferences[key]}
+            required=""
+          />
+        </label>
+      )
+    }
+  }
   return (
     <>
+      {/*
       <div className="rw-segment">
         <header className="rw-segment-header">
           <h2 className="rw-heading rw-heading-secondary">
@@ -97,6 +128,112 @@ const User = ({ user }) => {
           </button>
         )}
       </nav>
+        */}
+      <div className="rounded-md">
+        <form id="payment-form" method="POST" action="">
+          <section>
+            <h2 className="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2">
+              {user.name}
+            </h2>
+            <fieldset className="mb-3 bg-white shadow-lg rounded text-gray-600">
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Id</span>
+                <input
+                  name="name"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="System Generated"
+                  readOnly=""
+                  value={user.id}
+                />
+              </label>
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Name</span>
+                <input
+                  name="email"
+                  type="email"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="try@example.com"
+                  required=""
+                  value={user.name}
+                />
+              </label>
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Email</span>
+                <input
+                  name="email"
+                  type="email"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="try@example.com"
+                  required=""
+                  value={user.email}
+                />
+              </label>
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Updated</span>
+                <input
+                  name="createdAt"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="YYYY-MM-DD HH:MM:SS"
+                  required=""
+                  value={user.updatedAt}
+                />
+              </label>
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Created</span>
+                <input
+                  name="createdAt"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="YYYY-MM-DD HH:MM:SS"
+                  required=""
+                  value={user.createdAt}
+                />
+              </label>
+              <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+                <span className="text-right px-2 w-1/6">Password</span>
+                <input
+                  name="city"
+                  className="focus:outline-none px-3 w-5/6"
+                  placeholder="Only put something here if you want to change it"
+                />
+              </label>
+            </fieldset>
+          </section>
+        </form>
+      </div>
+      <div className="rounded-md">
+        <section>
+          <h2 className="uppercase tracking-wide text-lg font-semibold text-gray-700 my-2">
+            Preferences
+          </h2>
+          <fieldset className="mb-3 bg-white shadow-lg rounded text-gray-600">
+            <label className="flex border-b border-gray-200 h-12 py-3 items-center">
+              <span className="text-right px-2">Card</span>
+              <input
+                name="card"
+                className="focus:outline-none px-3 w-full"
+                placeholder="Card number MM/YY CVC"
+                required=""
+              />
+            </label>
+            {preferenceFields}
+          </fieldset>
+        </section>
+      </div>
+      <button
+        className="submit-button px-4 py-3 rounded-full bg-blue-400 hover:bg-blue-700 text-white focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
+        to={routes.editUser({ id: user.id })}
+      >
+        Save
+      </button>
+      {hasRole(['userDelete', 'admin']) && (
+        <button
+          type="button"
+          className="submit-button px-4 py-3 rounded-full bg-red-400 hover:bg-red-700 text-white focus:ring focus:outline-none w-full text-xl font-semibold transition-colors"
+          onClick={() => onDeleteClick(user.id)}
+        >
+          Delete
+        </button>
+      )}
     </>
   )
 }
--- web/src/components/User/UsersCell/UsersCell.js
+++ web/src/components/User/UsersCell/UsersCell.js
@@ -1,7 +1,6 @@
 import { Link, routes } from '@redwoodjs/router'
 import Table from 'src/components/Table/Table'
 import { UPDATE_USER_MUTATION } from 'src/components/User/EditUserCell'
-
 export const QUERY = gql`
   query FindUsers {
     users {
@@ -72,12 +71,196 @@ export const Success = ({ users }) => {
   `
   return (
     <>
-      <Table
+      {/*<Table
         data={users}
         meta={meta}
         query={QUERY}
         deleteMutation={DELETE_USER_MUTATION}
-      />
+      />*/}
+      <div className="bg-white pb-4 px-4 rounded-md w-full">
+        <div className="flex justify-between w-full pt-6 ">
+          <p className="ml-3"> Users Table</p>
+          <svg
+            width="14"
+            height="4"
+            viewBox="0 0 14 4"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.4">
+              <circle cx="2.19796" cy="1.80139" r="1.38611" fill="#222222" />
+              <circle cx="11.9013" cy="1.80115" r="1.38611" fill="#222222" />
+              <circle cx="7.04991" cy="1.80115" r="1.38611" fill="#222222" />
+            </g>
+          </svg>
+        </div>
+        <div className="w-full flex justify-end px-2 mt-2">
+          <div className="w-full sm:w-64 inline-block relative ">
+            <input
+              type=""
+              name=""
+              className="leading-snug border border-gray-300 block w-full appearance-none bg-gray-100 text-sm text-gray-600 py-1 px-4 pl-8 rounded-lg"
+              placeholder="Search"
+            />
+
+            <div className="pointer-events-none absolute pl-3 inset-y-0 left-0 flex items-center px-2 text-gray-300">
+              <svg
+                className="fill-current h-3 w-3"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 511.999 511.999"
+              >
+                <path d="M508.874 478.708L360.142 329.976c28.21-34.827 45.191-79.103 45.191-127.309C405.333 90.917 314.416 0 202.666 0S0 90.917 0 202.667s90.917 202.667 202.667 202.667c48.206 0 92.482-16.982 127.309-45.191l148.732 148.732c4.167 4.165 10.919 4.165 15.086 0l15.081-15.082c4.165-4.166 4.165-10.92-.001-15.085zM202.667 362.667c-88.229 0-160-71.771-160-160s71.771-160 160-160 160 71.771 160 160-71.771 160-160 160z" />
+              </svg>
+            </div>
+          </div>
+        </div>
+        <div className="overflow-x-auto mt-6">
+          <table className="table-auto border-collapse w-full">
+            <thead>
+              <tr className="rounded-lg text-sm font-medium text-gray-700 text-left">
+                <th className="px-4 py-2 ">Name</th>
+                <th className="px-4 py-2 ">Updated</th>
+                <th className="px-4 py-2 ">Created</th>
+                <th className="px-4 py-2 "></th>
+              </tr>
+            </thead>
+            <tbody className="">
+              {users.map((user) => {
+                return (
+                  <tr key={user.id}>
+                    <td className="border px-4 py-2 align-bottom">
+                      <Link to={routes.user({ id: user.id })}>
+                        <div className="flex">
+                          <div className="flex-shrink-0 w-10 h-10">
+                            <img
+                              className="w-full h-full rounded-full"
+                              src={`http://www.gravatar.com/avatar/${user.md5Email}?s=260&d=mp`}
+                              alt={user.name}
+                            />
+                          </div>
+                          <div className="ml-3">
+                            <p className="text-gray-900 whitespace-no-wrap">
+                              {user.name}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                    </td>
+                    <td className="border px-4 py-2">{user.updatedAt}</td>
+                    <td className="border px-4 py-2">{user.createdAt}</td>
+                    <td className="border px-4 py-2">
+                      <div className="flex gap-2">
+                        <Link
+                          to={routes.user({ id: user.id })}
+                          className="text-sm text-blue-500 hover:text-blue-700"
+                        >
+                          <span className="text-blue-300 hover:text-blue-700">
+                            <svg
+                              className="h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 20 20"
+                              stroke="currentColor"
+                            >
+                              <path d="M8.627,7.885C8.499,8.388,7.873,8.101,8.13,8.177L4.12,7.143c-0.218-0.057-0.351-0.28-0.293-0.498c0.057-0.218,0.279-0.351,0.497-0.294l4.011,1.037C8.552,7.444,8.685,7.667,8.627,7.885 M8.334,10.123L4.323,9.086C4.105,9.031,3.883,9.162,3.826,9.38C3.769,9.598,3.901,9.82,4.12,9.877l4.01,1.037c-0.262-0.062,0.373,0.192,0.497-0.294C8.685,10.401,8.552,10.18,8.334,10.123 M7.131,12.507L4.323,11.78c-0.218-0.057-0.44,0.076-0.497,0.295c-0.057,0.218,0.075,0.439,0.293,0.495l2.809,0.726c-0.265-0.062,0.37,0.193,0.495-0.293C7.48,12.784,7.35,12.562,7.131,12.507M18.159,3.677v10.701c0,0.186-0.126,0.348-0.306,0.393l-7.755,1.948c-0.07,0.016-0.134,0.016-0.204,0l-7.748-1.948c-0.179-0.045-0.306-0.207-0.306-0.393V3.677c0-0.267,0.249-0.461,0.509-0.396l7.646,1.921l7.654-1.921C17.91,3.216,18.159,3.41,18.159,3.677 M9.589,5.939L2.656,4.203v9.857l6.933,1.737V5.939z M17.344,4.203l-6.939,1.736v9.859l6.939-1.737V4.203z M16.168,6.645c-0.058-0.218-0.279-0.351-0.498-0.294l-4.011,1.037c-0.218,0.057-0.351,0.28-0.293,0.498c0.128,0.503,0.755,0.216,0.498,0.292l4.009-1.034C16.092,7.085,16.225,6.863,16.168,6.645 M16.168,9.38c-0.058-0.218-0.279-0.349-0.498-0.294l-4.011,1.036c-0.218,0.057-0.351,0.279-0.293,0.498c0.124,0.486,0.759,0.232,0.498,0.294l4.009-1.037C16.092,9.82,16.225,9.598,16.168,9.38 M14.963,12.385c-0.055-0.219-0.276-0.35-0.495-0.294l-2.809,0.726c-0.218,0.056-0.351,0.279-0.293,0.496c0.127,0.506,0.755,0.218,0.498,0.293l2.807-0.723C14.89,12.825,15.021,12.603,14.963,12.385"></path>
+                            </svg>
+                          </span>
+                        </Link>
+                        <Link
+                          to={routes.editUser({ id: user.id })}
+                          className="text-sm text-blue-500 hover:text-blue-700"
+                        >
+                          {' '}
+                          <span className="text-green-300 hover:text-green-700">
+                            <svg
+                              className="h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 20 20"
+                              stroke="currentColor"
+                            >
+                              <path d="M18.303,4.742l-1.454-1.455c-0.171-0.171-0.475-0.171-0.646,0l-3.061,3.064H2.019c-0.251,0-0.457,0.205-0.457,0.456v9.578c0,0.251,0.206,0.456,0.457,0.456h13.683c0.252,0,0.457-0.205,0.457-0.456V7.533l2.144-2.146C18.481,5.208,18.483,4.917,18.303,4.742 M15.258,15.929H2.476V7.263h9.754L9.695,9.792c-0.057,0.057-0.101,0.13-0.119,0.212L9.18,11.36h-3.98c-0.251,0-0.457,0.205-0.457,0.456c0,0.253,0.205,0.456,0.457,0.456h4.336c0.023,0,0.899,0.02,1.498-0.127c0.312-0.077,0.55-0.137,0.55-0.137c0.08-0.018,0.155-0.059,0.212-0.118l3.463-3.443V15.929z M11.241,11.156l-1.078,0.267l0.267-1.076l6.097-6.091l0.808,0.808L11.241,11.156z"></path>
+                            </svg>
+                          </span>
+                        </Link>
+
+                        <Link
+                          to={routes.editUser({ id: user.id })}
+                          className="text-sm text-blue-500 hover:text-blue-700"
+                        >
+                          <div className="text-red-300 hover:text-red-700">
+                            <svg
+                              className="h-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 20 20"
+                              stroke="currentColor"
+                            >
+                              <path d="M17.114,3.923h-4.589V2.427c0-0.252-0.207-0.459-0.46-0.459H7.935c-0.252,0-0.459,0.207-0.459,0.459v1.496h-4.59c-0.252,0-0.459,0.205-0.459,0.459c0,0.252,0.207,0.459,0.459,0.459h1.51v12.732c0,0.252,0.207,0.459,0.459,0.459h10.29c0.254,0,0.459-0.207,0.459-0.459V4.841h1.511c0.252,0,0.459-0.207,0.459-0.459C17.573,4.127,17.366,3.923,17.114,3.923M8.394,2.886h3.214v0.918H8.394V2.886z M14.686,17.114H5.314V4.841h9.372V17.114z M12.525,7.306v7.344c0,0.252-0.207,0.459-0.46,0.459s-0.458-0.207-0.458-0.459V7.306c0-0.254,0.205-0.459,0.458-0.459S12.525,7.051,12.525,7.306M8.394,7.306v7.344c0,0.252-0.207,0.459-0.459,0.459s-0.459-0.207-0.459-0.459V7.306c0-0.254,0.207-0.459,0.459-0.459S8.394,7.051,8.394,7.306"></path>
+                            </svg>
+                          </div>
+                        </Link>
+                      </div>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+        <div
+          id="pagination"
+          className="w-full flex justify-center border-t border-gray-100 pt-4 items-center"
+        >
+          <svg
+            className="h-6 w-6"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <g opacity="0.4">
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M9 12C9 12.2652 9.10536 12.5196 9.29289 12.7071L13.2929 16.7072C13.6834 17.0977 14.3166 17.0977 14.7071 16.7072C15.0977 16.3167 15.0977 15.6835 14.7071 15.293L11.4142 12L14.7071 8.70712C15.0977 8.31659 15.0977 7.68343 14.7071 7.29289C14.3166 6.90237 13.6834 6.90237 13.2929 7.29289L9.29289 11.2929C9.10536 11.4804 9 11.7348 9 12Z"
+                fill="#2C2C2C"
+              />
+            </g>
+          </svg>
+
+          <p className="leading-relaxed cursor-pointer mx-2 text-blue-600 hover:text-blue-600 text-sm">
+            1
+          </p>
+          <p className="leading-relaxed cursor-pointer mx-2 text-sm hover:text-blue-600">
+            2
+          </p>
+          <p className="leading-relaxed cursor-pointer mx-2 text-sm hover:text-blue-600">
+            {' '}
+            3{' '}
+          </p>
+          <p className="leading-relaxed cursor-pointer mx-2 text-sm hover:text-blue-600">
+            {' '}
+            4{' '}
+          </p>
+          <svg
+            className="h-6 w-6"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              clipRule="evenodd"
+              d="M15 12C15 11.7348 14.8946 11.4804 14.7071 11.2929L10.7071 7.2929C10.3166 6.9024 9.6834 6.9024 9.2929 7.2929C8.9024 7.6834 8.9024 8.3166 9.2929 8.7071L12.5858 12L9.2929 15.2929C8.9024 15.6834 8.9024 16.3166 9.2929 16.7071C9.6834 17.0976 10.3166 17.0976 10.7071 16.7071L14.7071 12.7071C14.8946 12.5196 15 12.2652 15 12Z"
+              fill="#18A0FB"
+            />
+          </svg>
+        </div>
+      </div>
     </>
   )
 }
