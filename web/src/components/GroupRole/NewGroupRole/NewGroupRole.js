de8f0d71da6bca8ce817ca902de9d94a6dc514db
Jace Benson <638764+jacebenson@users.noreply.github.com>
2021-11-21 21:08:13 -0600

    working on new form

--- web/src/components/GroupRole/NewGroupRole/NewGroupRole.js
+++ web/src/components/GroupRole/NewGroupRole/NewGroupRole.js
@@ -1,11 +1,17 @@
 import { useMutation } from '@redwoodjs/web'
 import { toast } from '@redwoodjs/web/toast'
 import { navigate, routes } from '@redwoodjs/router'
-import GroupRoleForm from 'src/components/GroupRole/GroupRoleForm'
-
-const CREATE_GROUP_ROLE_MUTATION = gql`
+import FormComponent from 'src/components/FormComponent'
+export const CREATE_GROUP_ROLE_MUTATION = gql`
   mutation CreateGroupRoleMutation($input: CreateGroupRoleInput!) {
     createGroupRole(input: $input) {
+      id
+    }
+  }
+`
+const DELETE_GROUP_ROLE_MUTATION = gql`
+  mutation DeleteGroupRoleMutation($id: Int!) {
+    deleteGroupRole(id: $id) {
       id
     }
   }
@@ -26,17 +32,90 @@ const NewGroupRole = () => {
     const castInput = Object.assign(input, { groupId: parseInt(input.groupId) })
     createGroupRole({ variables: { input: castInput } })
   }
+  const [deleteGroup] = useMutation(DELETE_GROUP_ROLE_MUTATION, {
+    onCompleted: () => {
+      toast.success('Group role deleted')
+      navigate(routes.users())
+    },
+  })
 
+  const onDelete = (id) => {
+    if (confirm('Are you sure you want to delete group role ' + id + '?')) {
+      deleteGroup({ variables: { id } })
+    }
+  }
+  const fields = [
+    {
+      name: 'groupId',
+      prettyName: 'Group',
+      type: 'reference',
+      display: 'name',
+      value: 'id',
+      data: [
+        {
+          name: 'Administrators',
+          id: 1,
+        },
+      ],
+    },
+    {
+      name: 'role',
+      prettyName: 'Role',
+      type: 'reference',
+      display: 'name',
+      value: 'name',
+      data: [
+        { name: 'userCreate' },
+        { name: 'userRead' },
+        { name: 'userUpdate' },
+        { name: 'userDelete' },
+        { name: 'groupCreate' },
+        { name: 'groupRead' },
+        { name: 'groupUpdate' },
+        { name: 'groupDelete' },
+        { name: 'groupMemberCreate' },
+        { name: 'groupMemberRead' },
+        { name: 'groupMemberUpdate' },
+        { name: 'groupMemberDelete' },
+        { name: 'groupRoleCreate' },
+        { name: 'groupRoleRead' },
+        { name: 'groupRoleUpdate' },
+        { name: 'groupRoleDelete' },
+        { name: 'admin' },
+      ],
+    },
+  ]
+  const roles = {
+    update: ['groupUpdate'],
+    delete: ['groupDelete'],
+  }
+
+  const mutations = {
+    deleteRecord: DELETE_GROUP_ROLE_MUTATION,
+  }
   return (
-    <div className="rw-segment">
-      <header className="rw-segment-header">
-        <h2 className="rw-heading rw-heading-secondary">New GroupRole</h2>
-      </header>
-      <div className="rw-segment-main">
-        <GroupRoleForm onSave={onSave} loading={loading} error={error} />
-      </div>
-    </div>
+    <FormComponent
+      fields={fields}
+      roles={roles}
+      onSave={onSave}
+      onDelete={onDelete}
+      mutations={mutations}
+      loading={loading}
+      error={error}
+      returnLink={routes.groupRoles()}
+    />
   )
+
+  //  return (
+  //    <div className="rw-segment">
+  //      <header className="rw-segment-header">
+  //        <h2 className="rw-heading rw-heading-secondary">New GroupRole</h2>
+  //      </header>
+  //      <div className="rw-segment-main">
+  //        <GroupRoleForm onSave={onSave} loading={loading} error={error} />
+  //      </div>
+  //    </div>
+  //  )
 }
 
 export default NewGroupRole
--- web/src/pages/GroupRole/GroupRolePage/GroupRolePage.js
+++ web/src/pages/GroupRole/GroupRolePage/GroupRolePage.js
@@ -1,7 +1,9 @@
-import GroupRoleCell from 'src/components/GroupRole/GroupRoleCell'
-
-const GroupRolePage = ({ id }) => {
-  return <GroupRoleCell id={id} />
-}
-
-export default GroupRolePage
+//import GroupRoleCell from 'src/components/GroupRole/GroupRoleCell'
+//
+//const GroupRolePage = ({ id }) => {
+//  //return <GroupRoleCell id={id} />
+//  return <>is this used? {id}</>
+//}
+//
+//export default GroupRolePage
+//
