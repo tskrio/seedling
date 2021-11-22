a3aea9469e67afa112cc8d81b0f02a9c5b7dd99d
Jace Benson <638764+jacebenson@users.noreply.github.com>
2021-11-21 21:11:48 -0600

    removed groupmemberpage

--- web/src/pages/Group/GroupPage/GroupPage.js
+++ web/src/pages/Group/GroupPage/GroupPage.js
@@ -1,7 +1,12 @@
 import GroupCell from 'src/components/Group/GroupCell'
 
 const GroupPage = ({ id }) => {
-  return <GroupCell id={id} />
+  return (
+    <>
+      is this used?
+      <GroupCell id={id} />
+    </>
+  )
 }
 
 export default GroupPage
