f266c5cd54e3ffd256f66f9bc1008b3d0264dba9
Jace Benson <638764+jacebenson@users.noreply.github.com>
2022-01-21 21:00:20 -0600

    remove other unsplash image

--- web/src/components/SidebarWithHeader/SidebarWithHeader.js
+++ web/src/components/SidebarWithHeader/SidebarWithHeader.js
@@ -243,12 +243,6 @@ const MobileNav = ({ brand, onOpen, ...rest }) => {
               _focus={{ boxShadow: 'none' }}
             >
               <HStack>
-                <Avatar
-                  size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
-                />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
                   alignItems="flex-start"
